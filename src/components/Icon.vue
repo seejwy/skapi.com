9c9b5f58c34faaee3e809a5dbdb55c0c85cad1d2
seejwy <jinyoon@broadwayinc.com>
2023-01-12 16:15:34 +0900

    Remove unused variable

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -4,7 +4,6 @@ import { ref, h, useSlots } from 'vue'
 export default{
   setup() {
     const slots = useSlots();
-    const count = ref(1);
     return () => h('svg', [
       h('use', {
         'xlink:href': `/src/assets/img/icons/sprite.svg#${slots.default()[0].children}`,
