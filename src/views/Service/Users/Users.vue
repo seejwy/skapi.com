20b204063bcdc3976bcbca73e2b6b421ccad346a
seejwy <jinyoon@broadwayinc.com>
2023-01-24 11:36:37 +0900

    Change overflow settings
    This is because any dropdown inside the table gets cut off

--- src/views/Service/Users/Users.vue
+++ src/views/Service/Users/Users.vue
@@ -650,7 +650,6 @@ getUsers();
     margin-top: 45px;
     background-color: #434343;
     border-radius: 8px;
-    overflow: hidden;
     border: 1px solid rgba(255, 255, 255, 0.2);
 
     .table-actions {
@@ -660,6 +659,7 @@ getUsers();
         background: #434343;
         height: 52px;
         padding: 0 14px 0 20px;
+        border-radius: 8px;
 
         & > * {
             cursor: pointer;
