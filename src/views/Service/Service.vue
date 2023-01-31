a65fb4ca6620607f4a2f81efd546027c0de24adb
seejwy <jinyoon@broadwayinc.com>
2023-01-31 18:20:59 +0900

    Update service page

--- src/views/Service/Service.vue
+++ src/views/Service/Service.vue
@@ -7,44 +7,45 @@
 .container
     h2 Service Information
     .information-grid
-        .information-grid-item(v-for="info in informationGrid")
+        .information-grid-item(v-for="info in informationGrid" :class="{'span-2': info.span}")
             .name {{ info.name }}
-            .value {{ service[info.key] }}
+            .value(v-if="info.filter") {{ info.filter(service[info.key]) }}
+            .value(v-else) {{ service[info.key] }}
+
+.container
+    h2 Service Setting 
+    .setting-grid 
+        .setting-grid-item(v-for="setting in settingGrid")
+            .name {{ setting.name }}
+            .value {{  service[setting.key] }}
+        .setting-grid-item.actions
+            sui-button.line-button Edit
 .container
     h2 Manage your Service 
     .service-grid 
         .service-grid-item
-            div
+            .title
                 Icon user_setting
-                span.title Authentication
-            div something else
+                span Authentication
+            div Users are data that your service user's will store and read from your service database. 
+            RouterLink(:to="{name: 'users'}") Go to Users >
         .service-grid-item  
-            div
+            .title
                 Icon service
-                span.title Record
+                span Record
+            div Users are data that your service user's will store and read from your service database. 
+            RouterLink(:to="{name: 'records'}") Go to Records >
         .service-grid-item 
-            div
+            .title
                 Icon mail
-                span.title Email System
-
-.container
-    h2 Security 
-    p Well, this is how you start a service!!
-    div.action
-        sui-button.line-button Edit
-
-h1 Service Dashboard
-h4 {{ service.name }}
-hr
-ul
-    li
-        router-link(:to="{name:'records'}") Records
-    li
-        router-link(:to="{name:'users'}") Users
+                span Email System
+            div Users are data that your service user's will store and read from your service database. 
+            RouterLink(to="/") Go to Mail >
 
 </template>
 <script setup>
 import { inject, reactive } from 'vue';
+import { localeName, dateFormat } from '@/main';
 import Icon from '../../components/Icon.vue';
 
 let service = inject('service');
@@ -53,36 +54,39 @@ pageTitle.value = 'Service "' + service.value.name + '"'
 
 const informationGrid = reactive([
     {
-        name: 'Name of Service',
-        key: 'name'
-    },
-    {
-        name: 'Date Created',
-        key: 'timestamp'
+        name: 'User ID',
+        key: 'owner',
+        span: 2
     },
     {
         name: 'Service ID',
-        key: 'service'
+        key: 'service',
+        span: 2
+    },
+    {
+        name: 'Group',
+        key: 'group',
+        filter: (value) => {
+            return value == 1 ? 'Basic' : 'Premium'
+        }
     },
     {
         name: 'Service Location',
-        key: 'region'
+        key: 'region',
+        filter: (value) => {
+            return localeName(value);
+        }
     },
     {
-        name: 'CORS',
-        key: 'cors'
-    },
-    {
-        name: 'API Key',
-        key: 'api_key'
+        name: 'Date Created',
+        key: 'timestamp',
+        filter: (value) => {
+            return dateFormat(value).split(' ')[0];
+        }
     },
     {
         name: 'Storage Use',
         key: null
-    },
-    {
-        name: 'User ID',
-        key: 'owner'
     },
     {
         name: '# of Users',
@@ -92,14 +96,30 @@ const informationGrid = reactive([
         name: '# of Newsletter Sub',
         key: 'newsletter_subscribers'
     },
-    {
-        name: 'Group',
-        key: 'group'
-    },
+]);
 
-])
+const settingGrid = reactive([
+    {
+        name: 'Enable/Disable',
+        key: 'active'
+    },
+    {
+        name: 'Name of Service',
+        key: 'name'
+    },
+    {
+        name: 'CORS',
+        key: 'cors'
+    },
+    {
+        name: 'API Key',
+        key: 'api_key'
+    },
+]);
 </script>
 <style lang="less" scoped>
+@import '../../assets/variables.less';
+
 .container {
     padding: 40px;
     background: #434343;
@@ -113,7 +133,7 @@ const informationGrid = reactive([
 
     h2 {
         font-size: 20px;
-        margin-bottom: 32px;
+        margin-bottom: 40px;
     }
 
     .action {
@@ -122,18 +142,53 @@ const informationGrid = reactive([
 }
 
 .information-grid {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 160px);
+    column-gap: 16px;
+    row-gap: 40px;
 
     &-item {
         .name {
             font-size: 14px;
             color: rgba(255, 255, 255, 0.6);
+            margin-bottom: 8px;
         }
 
         .value {
+            font-weight: bold;
             color: rgba(255, 255, 255, 0.85);
+        }
+
+        &.span-2 {
+            grid-column: span 2;
+        }
+
+        @media @phone {
+            &.span-2 {
+                grid-column: span 1;
+            }
+        }
+    }
+}
+
+.setting-grid {
+    display: flex;
+    justify-content: space-between;
+    
+    &-item {
+        .name {
+            font-size: 14px;
+            color: rgba(255, 255, 255, 0.6);
+            margin-bottom: 8px;
+        }
+
+        .value {
+            font-weight: bold;
+            color: rgba(255, 255, 255, 0.85);
+        }
+
+        &.actions {
+            align-self: flex-end;
         }
     }
 }
@@ -144,6 +199,9 @@ const informationGrid = reactive([
     gap: 100px;
 
     &-item {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
         width: 100%;
         background: rgba(255, 255, 255, 0.1);
         padding: 20px;
@@ -151,9 +209,21 @@ const informationGrid = reactive([
 
         .title {
             display: inline-block;
-            margin-left: 8px;
-            vertical-align: middle;
             color: rgba(255, 255, 255, 0.85);
+            margin-bottom: 28px;
+
+            span {        
+                margin-left: 8px;    
+                vertical-align: middle;
+            }
+        }
+
+        a {
+            text-align: left;
+            margin-top: 40px;
+            color: rgba(255, 255, 255, 0.6);
+            font-size: 12px;
+            text-decoration: none;
         }
     }
 }
