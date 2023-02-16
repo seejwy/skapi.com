594bf2082fe4b1bb79321071b78f2e6537f61820
seejwy <jinyoon@broadwayinc.com>
2023-02-16 13:11:50 +0900

    Add shadow to login container

--- src/views/Main/Login.vue
+++ src/views/Main/Login.vue
@@ -125,7 +125,10 @@ function login() {
     width: 542px;
     max-width: 100%;
     margin-top: 60px;
-    
+    border: 1px solid #808080;
+    box-shadow: 4px 4px 12px rgba(0, 0, 0, 0.25);
+    border-radius: 8px;
+
     @media @tablet {
         margin: 40px auto 0;    
         background: #FFF;
