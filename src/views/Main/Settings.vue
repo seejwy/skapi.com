a672f6c742e1c3860b05e0d2a756e906d05d397a
seejwy <jinyoon@broadwayinc.com>
2023-02-21 18:24:54 +0900

    Settings layout

--- src/views/Main/Settings.vue
+++ src/views/Main/Settings.vue
@@ -2,7 +2,35 @@
 div(v-if='!state?.connection')
     // is loading...
 div(v-else-if="state?.user")
-    h1 Account Settings
+    .page-header.head-space-helper
+        h1 Account Settings
+
+    .settings
+        .title Name
+        .value(v-if="state.viewport === 'desktop'") {{  state.user.name }}
+        .actions
+            Icon pencil 
+            span.hideOnTablet Edit
+        .mobile-value(v-if="state.viewport === 'mobile'") {{  state.user.name }}
+        hr
+        .title Email
+        .value(v-if="state.viewport === 'desktop'") {{  state.user.name }}
+        .actions
+            sui-button.line-button Change Email
+        .mobile-value(v-if="state.viewport === 'mobile'") {{  state.user.name }}
+        hr   
+        .title Subscription
+        .value(v-if="state.viewport === 'desktop'") {{  state.user.name }}
+        .actions
+            Icon pencil 
+            span.hideOnTablet Edit
+        .mobile-value(v-if="state.viewport === 'mobile'") {{  state.user.name }}
+        hr
+        .title Password
+        .value(v-if="state.viewport === 'desktop'") {{  state.user.name }}
+        .actions
+            sui-button.line-button Change Password
+        .mobile-value(v-if="state.viewport === 'mobile'") {{  state.user.name }}
     Transition(name="toast")
         .toast(v-if="state.user && !state.user.email_verified && state.showVerificationNotification")
             Icon warning_bell
@@ -41,4 +69,71 @@ onUpdated(() => {
 
 <style lang="less" scoped>
 @import '@/assets/variables.less';
+
+.page-header {
+    @media @tablet {
+        text-align: center;
+    }
+
+    h1 {
+        display: block;
+    }
+}
+.settings {
+    display: grid;
+    grid-template-columns: auto 1fr 1fr ;
+    align-items: center;
+    column-gap: 80px;
+
+    & > .title {
+        font-weight: bold;
+        padding: 28px 0;
+    }
+
+    .actions {
+        justify-self: end;
+        color: var(--primary-color);
+        cursor: pointer;
+
+        .line-button {
+            color: var(--primary-color);
+            white-space: nowrap;
+        }
+    }
+
+    hr {    
+        grid-column: span 3;
+        width: 100%;
+        border: 1px solid rgba(0, 0, 0, 0.04);
+        box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.06);
+    }
+    @media @tablet {
+        grid-template-columns: auto 1fr ;
+        column-gap: 0;
+
+        & > .title {
+            height: auto;
+            .actions {
+                display: block;
+            }
+        }
+
+        .mobile-value {
+            grid-column: span 2;
+            padding-bottom: 28px;
+        }
+        
+
+        hr {    
+            grid-column: span 2;
+            margin:  0 -16px;
+            width: 100vw;
+        }
+    }
+    @media @phone {
+        hr {
+            margin: 0 -8px;
+        }
+    }
+}
 </style>
