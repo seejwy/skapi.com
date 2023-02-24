e305623c30e88b4f4a9766cc14eb79377075e41c
seejwy <jinyoon@broadwayinc.com>
2023-02-24 11:14:13 +0900

    Centralize form containers

--- src/views/Main/ForgotPassword.vue
+++ src/views/Main/ForgotPassword.vue
@@ -177,6 +177,8 @@ const changePassword = () => {
     align-items: center;
     justify-content: center;
     padding: 60px 0;
+    min-height: calc(100vh - 140px);
+
     @media @tablet {
         align-items: flex-start;
         padding: 0;
@@ -192,6 +194,7 @@ const changePassword = () => {
     border: 1px solid #808080;
     box-shadow: 4px 4px 12px rgba(0, 0, 0, 0.25);
     border-radius: 8px;
+    margin-top: 60px;
 
     @media @tablet {
         width: 100%;
