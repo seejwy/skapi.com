51fe35566412a46a6b427da22090eaa596eb600d
seejwy <jinyoon@broadwayinc.com>
2023-02-27 16:40:03 +0900

    Move icons to public folder

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -6,8 +6,8 @@ export default{
     const slots = useSlots();
     return () => h('svg', [
       h('use', {
-        'xlink:href': `/src/assets/img/icons/sprite.svg#${slots.default()[0].children}`,
-        href: `/src/assets/img/icons/sprite.svg#${slots.default()[0].children}`, innerHTML: slots.default()[0].children
+        'xlink:href': `${window.location.origin}/assets/img/icons/sprite.svg#${slots.default()[0].children}`,
+        href: `${window.location.origin}/assets/img/icons/sprite.svg#${slots.default()[0].children}`, innerHTML: slots.default()[0].children
       })
     ])
   }
