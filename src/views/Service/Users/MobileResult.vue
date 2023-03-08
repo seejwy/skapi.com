7a9526287696b7781910801c3eb04eebb7b6efdf
seejwy <jinyoon@broadwayinc.com>
2023-03-08 10:29:02 +0900

    Remove block from user table header in mobile

--- src/views/Service/Users/MobileResult.vue
+++ src/views/Service/Users/MobileResult.vue
@@ -10,7 +10,8 @@ SearchNavBar(v-if='viewport === "mobile"')
                 span Headers
                 Icon down2
             sui-select(:value="mobileVisibleField" @change="(e) => mobileVisibleField = e.target.value")
-                option(v-for="(field, key) in visibleFields" :value="key") {{  field.text  }}
+                template(v-for="(field, key) in visibleFields" :value="key")
+                    option(v-if="key !== 'suspended'") {{  field.text  }}
         Icon(v-if="viewport === 'desktop'" :class="{'animation-rotation': fetchingData}" @click="getUsers") refresh
         .actions(v-if="viewport === 'mobile'")
             sui-button.icon-button(@click="blockUsers" :disabled="selectedUsers.length === 0 || null")
