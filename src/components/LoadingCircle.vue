04b704c028b67abfa3c9b2927d376bd9d1bfa0e9
gyupp <kkb75281@gmail.com>
2023-04-25 17:03:05 +0900

    Create loading icon

---
+++ src/components/LoadingCircle.vue
@@ -0,0 +1,113 @@
+<template lang="pug">
+.content_box
+    .circle
+        .box
+    .background_box.b1 
+        .background.c1
+    .background_box.b2 
+        .background.c2 
+    .background_box.b3
+        .background.c3 
+    .background_box.b4
+        .background.c4
+</template>
+<script setup>
+
+</script>
+<style lang="less" scoped>
+.content_box {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    position: absolute;
+    box-sizing: border-box;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: auto;
+    width: 67px;
+    height: 67px;
+    border-radius: 50%;
+    background: #fff;
+    animation: rotateS .8s linear infinite;
+
+    @keyframes rotateS {
+        50% {
+            transform: rotate(180deg);
+        }
+
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+    .circle {
+        position: absolute;
+        width: 8px;
+        height: 50%;
+        z-index: 2;
+        transform-origin: 50% 100%;
+        transform: rotate(0deg);
+
+        .box {
+            position: absolute;
+            top: 0px;
+            width: 8px;
+            height: 8px;
+            border-radius: 50%;
+            background: rgba(41, 63, 230, 1);
+        }
+    }
+    .background_box {
+        position: relative;
+        width: 50%;
+        height: 50%;
+        box-sizing: border-box;
+
+        &.b1 {
+            border-radius: 100% 0 0 0/ 100% 0 0 0;
+            background: linear-gradient(rgba(41, 63, 230, 1), rgba(41, 63, 230, 0.8));
+        }
+        &.b2 {
+            border-radius: 0 100% 0 0/ 0 100% 0 0;
+            background: linear-gradient(transparent, transparent);
+        }
+        &.b3 {
+            border-radius: 0 0 0 100%/ 0 0 0 100%;
+            background: linear-gradient(rgba(41, 63, 230, 0.8), rgba(41, 63, 230, 0.4));
+        }
+        &.b4 {
+            border-radius: 0 0 100% 0/ 0 0 100% 0;
+            background: linear-gradient(transparent, rgba(41, 63, 230, 0.45));
+        }
+        .background {
+            position: absolute;
+            width: calc(100% - 8px);
+            height: calc(100% - 8px);
+            background: #fff;
+            border: none;
+
+            &.c1 {
+                right: 0;
+                bottom: 0;
+                border-radius: 100% 0 0 0/ 100% 0 0 0;
+            }
+            &.c2 {
+                left: 0;
+                bottom: 0;
+                border-radius: 0 100% 0 0/ 0 100% 0 0;
+            }
+            &.c3 {
+                right: 0;
+                top: 0;
+                border-radius: 0 0 0 100%/ 0 0 0 100%;
+            }
+            &.c4 {
+                left: 0;
+                top: 0;
+                border-radius: 0 0 100% 0/ 0 0 100% 0;
+            }
+        }
+    }
+}
+</style>
