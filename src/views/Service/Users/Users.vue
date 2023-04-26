2589252622232c5a223d8a5b2dc9cd1c264c00a7
seejwy <jinyoon@broadwayinc.com>
2023-04-26 15:36:32 +0900

    Remove incorrect use of value in template

--- src/views/Service/Users/Users.vue
+++ src/views/Service/Users/Users.vue
@@ -2,7 +2,7 @@
 SearchNavBar(v-if="route.query.search && viewport === 'mobile'")
     div {{ mobilePageTitle }}
     template(v-slot:right) 
-        Icon.showOnTablet.placeholder-icon(@click="()=>{ searchResult=null; currentSelectedRecordPage=0; currentSelectedRecordBatch=0; router.push({name: 'mobileSearchUser'}); getUsers(true); serviceUsers.value = null; }") X2
+        Icon.showOnTablet.placeholder-icon(@click="()=>{ searchResult=null; currentSelectedRecordPage=0; currentSelectedRecordBatch=0; router.push({name: 'mobileSearchUser'}); getUsers(true); serviceUsers = null; }") X2
 .page-header.head-space-helper(v-if="viewport === 'desktop' || !route.query.search")
     h1 Users
     p Users are data that your service user's will store and read from your service database. All records are organized by table names and restrictions. With additional query points such as index names and tags, references, you can have more flexible option when fetching the records.
