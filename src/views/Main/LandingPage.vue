25772dfa0947921c963072f2a92033da3629ec2f
seejwy <jinyoon@broadwayinc.com>
2023-05-24 14:15:43 +0900

    Add video to landing page

--- src/views/Main/LandingPage.vue
+++ src/views/Main/LandingPage.vue
@@ -34,7 +34,7 @@ section.sectionBox
                 //-     button.readBtn Read Document
 section.sectionBox.showVideo
     .videoCont 
-        img(src="@/assets/img/icons/video.svg")
+        <iframe class="video" src="https://www.youtube.com/embed/c57Km96AHeg?&rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 section.sectionBox.features
     .feaCont 
         .cardTit
