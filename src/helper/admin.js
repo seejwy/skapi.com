6b1145155657d562e6c94403f122e7e0bf01e7a3
seejwy <jinyoon@broadwayinc.com>
2023-06-16 11:28:55 +0900

    Split admin functions

---
+++ src/helper/admin.js
@@ -0,0 +1,26 @@
+import { state } from '@/main';
+
+export async function getServices(gs, serviceList) {
+    if (!(gs instanceof Promise) || !state.user) {
+        return;
+    }
+
+    try {
+        let services = await gs;
+        state.blockingPromise = false;
+        if (serviceList.value === null) {
+            serviceList.value = [];
+            for (let region in services) {
+                serviceList.value = [...serviceList.value, ...services[region]];
+            }
+
+            serviceList.value.sort((a, b) => a.timestamp > b.timestamp ? -1 : a.timestamp < b.timestamp ? 1 : 0);
+        }
+
+        return services;
+
+    } catch (err) {
+        serviceList.value = null;
+        throw err;
+    }
+}
